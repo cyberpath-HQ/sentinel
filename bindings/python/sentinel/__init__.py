03dc439b19b5d04fc83643c4e4bd461344fb521c
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-01-19 14:22:21 +0100

    feat: update module metadata and imports in sentinel Python bindings; include additional functions in __all__

--- bindings/python/sentinel/__init__.py
+++ bindings/python/sentinel/__init__.py
@@ -2,13 +2,33 @@
 Cyberpath Sentinel Python Bindings
 
 A filesystem-backed document DBMS written in Rust with Python bindings.
+
+Authors:
+    Cyberpath <support@cyberpath-hq.com>
+    Emanuele (Ebalo) Balsamo <emanuele.balsamo@cyberpath-hq.com>
+
+Repository: https://github.com/cyberpath-HQ/sentinel
+Documentation: https://sentinel.cyberpath-hq.com
 """
 
-from sentinel import Store, Collection, Document, QueryBuilder, QueryResult, VerificationOptions
+from sentinel_python import Store, Collection, Document, QueryBuilder, QueryResult, VerificationOptions
+from sentinel_python import hash_data_py, sign_hash_py, verify_signature_py
 
 __version__ = "2.0.1"
-__author__ = "Emanuele (Ebalo) Balsamo"
-__email__ = "emanuele.balsamo@cyberpath-hq.com"
+__author__ = "Cyberpath and Emanuele (Ebalo) Balsamo"
+__email__ = "support@cyberpath-hq.com"
 __license__ = "Apache-2.0"
+__repository__ = "https://github.com/cyberpath-HQ/sentinel"
+__documentation__ = "https://sentinel.cyberpath-hq.com"
 
-__all__ = ["Store", "Collection", "Document", "QueryBuilder", "QueryResult", "VerificationOptions"]
+__all__ = [
+    "Store",
+    "Collection",
+    "Document",
+    "QueryBuilder",
+    "QueryResult",
+    "VerificationOptions",
+    "hash_data_py",
+    "sign_hash_py",
+    "verify_signature_py",
+]
