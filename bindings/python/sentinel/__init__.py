16996dee9dac47314f32da6a6e2532b90b062e5f
ebalo55 <12586380+ebalo55@users.noreply.github.com>
2026-01-19 11:26:22 +0100

    feat: update Python bindings to include QueryBuilder, QueryResult, and VerificationOptions

--- bindings/python/sentinel/__init__.py
+++ bindings/python/sentinel/__init__.py
@@ -4,11 +4,11 @@ Cyberpath Sentinel Python Bindings
 A filesystem-backed document DBMS written in Rust with Python bindings.
 """
 
-from .sentinel_python import Store, Collection, Document
+from sentinel import Store, Collection, Document, QueryBuilder, QueryResult, VerificationOptions
 
 __version__ = "2.0.1"
 __author__ = "Emanuele (Ebalo) Balsamo"
 __email__ = "emanuele.balsamo@cyberpath-hq.com"
 __license__ = "Apache-2.0"
 
-__all__ = ["Store", "Collection", "Document"]
+__all__ = ["Store", "Collection", "Document", "QueryBuilder", "QueryResult", "VerificationOptions"]
